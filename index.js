b30bab10d5d61c29ead49a9bbe6f4675cdc03ec6
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-14 07:31:44 +0530

    crud operation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.listen(8080, ()=> {
     console.log("listening to the port: 8080");
