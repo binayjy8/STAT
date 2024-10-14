e6f52e1308a1239601db1af80743afd7ff253b87
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-14 07:32:53 +0530

    crud operation

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use();
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
