09d539d00c386debd730dd85fc554c51034ad247
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-18 11:43:39 +0530

    crud operation

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ app.get("/posts/:id", (req, res) => {
 app.patch("/posts/:id", (req, res) => {
     let { id } = req.params;
     let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
     res.send();
 });
 
