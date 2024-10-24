29c49bf1e19a896acf3d7fa7b9c2f7b46e8d8d8c
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-10-24 22:04:38 +0530

    crud operation

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get("/posts/fun", (req, res) => {
 });
 
 app.get("/posts/work", (req, res) => {
-    res.send("Always progress doing good be patitence a");
+    res.send("Always progress doing good be patitence an");
 });
 
 app.get("/posts/:id", (req, res) => {
